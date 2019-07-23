e074a7c2731bfce67f471f94f2587fb0040c2323
zhuzhaoxuan <onevicetone@outlook.com>
2019-07-23 17:32:59 +0800

    数据库connect修改

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,6 +1,13 @@
 module.exports = app => {
     const mongoose = require('mongoose')
-    mongoose.connect('mongodb://127.0.0.1:27.17/my_blog_server',{
+    //设置自动连接数据库，不会出现时间长了数据库断开
+    const options = {
+        server: {
+            auto_reconnect: true,
+            poolSize: 10
+        }
+    }
+    mongoose.connect('mongodb://blogAdmin:1067430126@172.18.58.208:27017/my_blog_server?authSource=my_blog_server',{
         useNewUrlParser:true
-    })
+    },options)
 }
